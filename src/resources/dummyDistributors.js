109ba52caaa999a4ac71ac44ab2a4832f7d43627
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2023-11-14 14:46:07 -0800

    Adds dummy files

---
+++ src/resources/dummyDistributors.js
@@ -0,0 +1,14 @@
+export default dummyDistributors = [
+  {
+    id: "3222d820-833f-11ee-b962-0242ac120002",
+    name: "Jamieson",
+    orderDay: "Tuesday",
+    deliveryDay: "Thursday",
+  },
+  {
+    id: "3977a02e-833f-11ee-b962-0242ac120002",
+    name: "Life",
+    orderDay: "Wednesday",
+    deliveryDay: "Friday",
+  },
+];
---
+++ src/resources/dummyOrders.js
@@ -0,0 +1,32 @@
+export default dummyOrders = [
+  {
+    id: "4802b6f6-833f-11ee-b962-0242ac120002",
+    product_id: "abc",
+    distributor_id: "abc",
+    status: "pending",
+  },
+  {
+    id: "4802b9da-833f-11ee-b962-0242ac120002",
+    product_id: "abc",
+    distributor_id: "abc",
+    status: "pending",
+  },
+  {
+    id: "4802beb2-833f-11ee-b962-0242ac120002",
+    product_id: "abc",
+    distributor_id: "abc",
+    status: "pending",
+  },
+  {
+    id: "4802bffc-833f-11ee-b962-0242ac120002",
+    product_id: "abc",
+    distributor_id: "abc",
+    status: "pending",
+  },
+  {
+    id: "4802c132-833f-11ee-b962-0242ac120002",
+    product_id: "abc",
+    distributor_id: "abc",
+    status: "pending",
+  },
+];
