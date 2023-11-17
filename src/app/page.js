e2d470189a33ae27c3c0344f16d570d155a224be
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2023-11-16 21:53:22 -0800

    Changes layout of Dashboard page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Layout, Space } from "antd";
+import { Button, Flex, Layout, Space } from "antd";
 import Sider from "antd/es/layout/Sider";
 import { Content, Footer, Header } from "antd/es/layout/layout";
 import CustomTable from "@/components/CustomTable";
@@ -16,8 +16,23 @@ const Home = () => {
   return (
     <Space direction="vertical" style={{ width: "100%" }} size={[0, 48]}>
       <h1>Dashboard</h1>
-      <CustomTable content={contentTable1} columns={columnTable1} />
-      <CustomTable content={contentTable2} columns={columnTable2} />
+      <button style={{ width: "200px" }}>Add new order</button>
+      <Flex justify="space-evenly">
+        <Space direction="vertical">
+          <Flex justify="space-between">
+            <h1>Table 1</h1>
+            <button>View all</button>
+          </Flex>
+          <CustomTable content={contentTable1} columns={columnTable1} />
+        </Space>
+        <Space direction="vertical">
+          <Flex justify="space-between">
+            <h1>Table 2</h1>
+            <button>View all</button>
+          </Flex>
+          <CustomTable content={contentTable2} columns={columnTable2} />
+        </Space>
+      </Flex>
     </Space>
   );
 };
