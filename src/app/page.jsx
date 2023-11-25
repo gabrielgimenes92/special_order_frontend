0f431a58f574b0cf6e1b082046b783cd7e383407
deborapatrao <deborapcorreia@gmail.com>
2023-11-24 22:48:41 -0800

    Initial setup and styling | Add sidebar

---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+import { Flex, Layout, Space } from "antd";
+import Sider from "antd/es/layout/Sider";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+
+export default function Home() {
+  return <></>;
+}
