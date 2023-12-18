cf14a4b8d066d3f2610a00e3f1c44cc00a6946c9
deborapatrao <deborapcorreia@gmail.com>
2023-12-18 15:59:21 -0800

    Add modal form

--- src/app/components/AddOrderModal.jsx
+++ src/app/components/AddOrderModal.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Modal } from "antd";
+import { Button, Modal, Form, Input, Radio } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 import { useState } from "react";
 
@@ -16,6 +16,13 @@ function AddOrderModal() {
     setIsModalOpen(false);
   };
 
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
     <>
       <Button
@@ -25,14 +32,107 @@ function AddOrderModal() {
         onClick={showModal}
       />
       <Modal
-        title="Basic Modal"
+        title="New Order"
         open={isModalOpen}
         onOk={handleOk}
         onCancel={handleCancel}
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <Form
+          name="addOrder"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 20,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <h5>Customer</h5>
+          <Form.Item
+            label="Customer Name"
+            name="customername"
+            rules={[{ required: true, message: "Please add customer name" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Phone Number"
+            name="phone"
+            rules={[{ required: true, message: "Please add a phone number" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item label="Email" name="email" rules={[{ required: false }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="Preferred contact" name="preferredcontact">
+            <Radio.Group>
+              <Radio value="phone">Phone</Radio>
+              <Radio value="email">Email</Radio>
+            </Radio.Group>
+          </Form.Item>
+
+          <Form.Item label="Frequency" name="frequency">
+            <Radio.Group>
+              <Radio value="onetime">One Time</Radio>
+              <Radio value="weekly">Weekly</Radio>
+              <Radio value="biweekly">Biweekly</Radio>
+              <Radio value="monthly">Monthly</Radio>
+              <Radio value="seniors">Seniors Day</Radio>
+            </Radio.Group>
+          </Form.Item>
+        </Form>
+
+        <Form
+          name="order"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 20,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+        >
+          <h5>Order</h5>
+
+          <Form.Item
+            label="Product Name"
+            name="productname"
+            rules={[{ required: true, message: "Please add product name" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Quantity"
+            name="quantity"
+            rules={[{ required: true, message: "Please add product quantity" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Brand"
+            name="brand"
+            rules={[{ required: true, message: "Please add product brand" }]}
+          >
+            <Input />
+          </Form.Item>
+        </Form>
       </Modal>
     </>
   );
